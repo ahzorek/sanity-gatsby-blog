795d6a5f549102c7b8a8a8d41f95678b4467392c
ahzorek <andrezorek@iMac-de-Andre.local>
2019-11-01 17:47:54 -0300

    new darkmode.

--- web/src/lib/dark-hook.js
+++ web/src/lib/dark-hook.js
@@ -1,27 +1,37 @@
 import { useState, useEffect } from 'react'
-import {isBrowser} from './helpers'
+//import {isBrowser} from './helpers'
 
 export default function useDark(){
-  const key = 'dark__mode'
-  const prefersDark = isBrowser() && 
-    matchMedia('(prefers-color-scheme: dark)').matches ? true : false
+  //const prefersDark = isBrowser() && 
+    //matchMedia('(prefers-color-scheme: dark)').matches ? true : false
   
-  const stateOfDark = isBrowser() &&
-    localStorage.getItem(key) !== null && 
-      localStorage.getItem(key) !== 'null'
-        ? JSON.parse(localStorage.getItem(key)) 
-        : false
-
-  const [state, setState] = useState(stateOfDark)
+  const key = 'dark__mode'
+  //const [isMounted, setMount] = useState(false)
+  const [state, setState] = useState(null)
   
   const setDarkState = () => {
       console.log("O estado de 'isDark' foi alterado de ", state, " para ", !state)
     setState(state => !state)
+    localStorage.setItem(key, !state)
+  }
+
+  useEffect(() => {
+    const stateOfDark = localStorage.getItem(key) !== null
+        ? JSON.parse(localStorage.getItem(key)) 
+        : window.matchMedia && matchMedia('(prefers-color-scheme: dark)').matches || false
+    
+    console.log("Mounting app, state is ", state)
+    
+    loadInitialState(stateOfDark)
+  },[])
+
+  const loadInitialState = (mountState) => {
+      setState(mountState)
+      console.log("Setting initial state as", mountState)
   }
 
   useEffect(()=> {
     window.addEventListener('storage', setDarkState)
-    localStorage.setItem(key, state)    
     return () => { 
       window.removeEventListener('storage', setDarkState) 
     }
